91f9984c06a6c11faf0953cc00b966be156a33ac
balajitnpscdrive-ship-it <balajitnpscdrive@gmail.com>
2026-03-28 00:45:10 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'dbpc-hub-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'icon-512.png'
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
